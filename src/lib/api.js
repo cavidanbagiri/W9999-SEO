c491a07de34455bcd1accf894baaad7b83834078
= <=>
2026-01-22 15:17:47 +0300

    Change the api

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const BASE = process.env.NEXT_PUBLIC_API_URL || 'https://www.w9999.app';
+const BASE = process.env.NEXT_PUBLIC_API_URL || 'https://duolingopocketserver.onrender.com';
 
 export async function getWord(langFrom, langTo, word) {
   const { data } = await axios.get(
@@ -10,21 +10,6 @@ export async function getWord(langFrom, langTo, word) {
   );
   return data;
 }
-
-// export async function getAllWordSlugs() {
-//   const { data } = await axios.get(`${BASE}/api/public/slugs`);
-//   return data; // [{lf:'en', lt:'ru', word:'hello'}, …]
-// }
-// export async function getAllWordSlugs() {
-//   try {
-//     const { data } = await axios.get(`${BASE}/api/public/slugs`);
-//     return Array.isArray(data) ? data : [];   // ← never break again
-//   } catch {
-//     return [];                                // ← build continues even if API down
-//   }
-// }
-
-
 
 export async function getAllWordSlugs() {
   try {
