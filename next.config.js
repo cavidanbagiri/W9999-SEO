65074a4f29676ae92768ac01e4a52189e98ddbb5
cavidan <cavidanbagiri@gmail.com>
2026-01-23 21:02:06 +0300

    Change config and add the output export

--- next.config.js
+++ next.config.js
@@ -1,36 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   output: 'export', // ← makes `next build` spit out plain HTML
-//   images: { unoptimized: true }, // required for export
-
-//   // send humans to the SPA while keeping HTML for Google
-//   async rewrites() {
-//     return [
-//       {
-//         source: '/app/:path*',
-//         destination: 'https://app.w9999.app/:path*',
-//       },
-//     ];
-//   },
-// };
-
-// module.exports = nextConfig;
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   output: 'export',
-//   images: { unoptimized: true },
-// };
-
-// module.exports = nextConfig;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: 'export',
+  output: 'export',
   images: { unoptimized: true },
 };
 
