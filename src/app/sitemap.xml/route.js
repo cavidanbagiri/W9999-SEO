dbcccfc622aa544c213f5df17d452f1e30ada993
= <=>
2026-01-23 18:21:50 +0300

    Change the router and clean the comment

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,26 +1,4 @@
-// import { getAllWordSlugs } from '@/lib/api';
 
-// export async function GET() {
-//   const slugs = await getAllWordSlugs();
-
-//   const urls = slugs.map(
-//     (s) => `
-//   <url>
-//     <loc>https://www.w9999.app/${s.lf}-${s.lt}/${s.word}</loc>
-//     <changefreq>weekly</changefreq>
-//     <priority>0.8</priority>
-//   </url>`
-//   );
-
-//   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-// <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-//   ${urls.join('')}
-// </urlset>`;
-
-//   return new Response(xml, {
-//     headers: { 'Content-Type': 'application/xml' },
-//   });
-// }
 import { getAllWordSlugs } from '@/lib/api';
 
 export const dynamic = 'force-static'; // ← ADD THIS LINE
