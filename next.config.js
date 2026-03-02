78bb7b77702f6e8a1418f9152700fff9f20410e1
= <=>
2026-03-02 18:41:34 +0300

    Rewrite render ready

--- next.config.js
+++ next.config.js
@@ -1,31 +1,16 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export",
-
-//   // Helps avoid issues on static hosts (Render) when using next/image
-//   images: {
-//     unoptimized: true,
-//   },
-
-//   // Optional: make builds less likely to fail on lint/type errors (remove if you want strict)
-//   eslint: { ignoreDuringBuilds: true },
-//   typescript: { ignoreBuildErrors: true },
-// };
-
-// // module.exports = nextConfig;
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export" - BUNU KALDIR VEYA YORUM SATIRI YAP
+  output: "export",
 
+  // Helps avoid issues on static hosts (Render) when using next/image
   images: {
-    unoptimized: true, // Eğer görseller sorun çıkarırsa bunu tut
+    unoptimized: true,
   },
 
+  // Optional: make builds less likely to fail on lint/type errors (remove if you want strict)
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
 
+// module.exports = nextConfig;
 export default nextConfig;
